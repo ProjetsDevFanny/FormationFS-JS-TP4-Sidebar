37a5134c288b6dd5eedcd50d0433a603880cbd8d
Fanny Simon <contact.fanny.simon@gmail.com>
2025-02-13 11:37:25 +0100

    Fond d'écran devient opaque quand sidebar open

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 
-// Au clic sur la croix, la sidebar se cache:
-
-closeBtn.addEventListener("click", function() {
-  sidebar.style.transform = "translateX(-12rem)";
-  closeBtn.style.visibility = "hidden";
-  openBtn.style.visibility = "visible";
-});
-
-
 // Au clic sur l'icone "bars", la sidebar apparaît:
 
-openBtn.addEventListener("click", function() {
-  sidebar.style.transform = "translateX(0rem)";
+openBtn.addEventListener("click", function () {
+  sidebar.style.transform = "translateX(12rem)";
   openBtn.style.visibility = "hidden";
   closeBtn.style.visibility = "visible";
+  main.style.opacity = "0.4";
 });
 
 
+// Au clic sur la croix, la sidebar se cache:
 
+closeBtn.addEventListener("click", function () {
+  sidebar.style.transform = "translateX(0rem)";
+  closeBtn.style.visibility = "hidden";
+  openBtn.style.visibility = "visible";
+  main.style.opacity = "1";
+});
+
+// Au click sur le main, la sidebar se cache
+
+main.addEventListener("click", function() {
+  sidebar.style.transform = "translateX(0rem)";
+  closeBtn.style.visibility = "hidden";
+  openBtn.style.visibility = "visible";
+  main.style.opacity = "1";
+});
 
 
